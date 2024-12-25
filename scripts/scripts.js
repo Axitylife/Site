dc2ed7f22c8300da2b7a0ee8e6f364e96dccf37b
M_Chernikovv <axityslife@gmail.com>
2024-12-25 17:38:06 +0300

    Easy Variable ShowMenu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,23 +3,19 @@ const burgerMenu = document.querySelector(".burger");
 const header = document.querySelector(".burgerMenuOpen");
 let selecterBurger = false;
 
-let newElement = document.createElement('div');
-newElement.textContent = 'Set Text';
 
 
 burgerMenu.onclick = function()
 {
     if(!selecterBurger)
         {
-            header.classList.add('test');
+            header.style.display = 'block';
             selecterBurger = true;
-            header.append(newElement);
         }
     else
         {
-            header.classList.remove('test');
+            header.style.display = 'none';
             selecterBurger = false;
-            header.removeChild(newElement);
         }
 };
 
